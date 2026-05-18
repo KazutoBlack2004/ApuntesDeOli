ada506cab73a2e9b68f6ef7af70bebee1989dd1e
Kazuto_Black <160081055+KazutoBlack2004@users.noreply.github.com>
2026-05-18 11:24:53 -0400

    fix: update base path logic to support deployment on Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,12 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 const isDev = process.env.npm_lifecycle_event === 'dev';
+const isVercel = !!process.env.VERCEL;
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://KazutoBlack2004.github.io',
-  base: isDev ? '/' : '/ApuntesDeOli',
+  base: (isDev || isVercel) ? '/' : '/ApuntesDeOli',
   integrations: [react()],
 
   vite: {
